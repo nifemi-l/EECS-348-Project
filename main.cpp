0128a8dde4c6b5a352077294ca044a9f645279eb
danielosw <65432065+danielosw@users.noreply.github.com>
2024-12-01 12:42:40 -0600

    fix typos

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 /*Daniel Van Dalsem*/
-#include <stdio.h>
 #include <string>
 #include <vector>
 #include "lexer.cpp"
 #include "parser.cpp"
 #include "interpreter.cpp"
-#include "errorhandling.h"
 using namespace std;
 //import lexer, parser, interpreter
 
@@ -19,14 +17,14 @@ while (true) {
 		vector<Token> tokens = lexer.generateTokens();
 		Parser parser = Parser(tokens);
 		//I am not sure what type this will be???
-		vector<Token> tree = Parser.parse();
+		vector<Token> tree = parser.parse();
 		// I don't think you can return null in c++ in this case so
 		if (false){
 			continue;
 		}
 		Interpreter interpreter = Interpreter();
 		// I also don't know what type this will be
-		float value = Interpreter.visit(tree);
+		float value = interpreter.visit(tree);
 		cout<<value<<endl;
 	}
 	catch (const std::exception& e) {
